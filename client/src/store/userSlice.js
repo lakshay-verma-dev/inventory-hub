f795a37b1ba95c308993eabe70d164a98688a9dd
Lakshay-frontend-Dev <vermaff495@gmail.com>
2025-01-17 14:44:22 +0530

    initialise store for user

--- client/src/store/userSlice.js
+++ client/src/store/userSlice.js
@@ -1,9 +1,13 @@
-// src/redux/userSlice.js
 import { createSlice } from "@reduxjs/toolkit";
 
+// Initial state for the user slice
 const initialState = {
   token: null,
-  user: null,
+  user: {
+    firstName: null,
+    lastName: null,
+    email: null,
+  },
 };
 
 const userSlice = createSlice({
@@ -11,12 +15,25 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {
-      state.token = action.payload.token;
-      state.user = action.payload.user;
+      const { token, user } = action.payload;
+      if (user) {
+        state.token = token || null;
+        state.user = {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        };
+      } else {
+        console.error("Invalid payload structure in setUser:", action.payload);
+      }
     },
     clearUser: (state) => {
       state.token = null;
-      state.user = null;
+      state.user = {
+        firstName: null,
+        lastName: null,
+        email: null,
+      };
     },
   },
 });
