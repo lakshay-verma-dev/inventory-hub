4d4f75265d65741d6f0e7b408c401ab82df961c3
Lakshay-frontend-Dev <vermaff495@gmail.com>
2025-01-20 15:56:21 +0530

    correct the api

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-const USER_API = `${API_BASE_URL}/auth`;
-const BOOK_API = `${API_BASE_URL}/book`;
-const PAYMENT_API = `${API_BASE_URL}/payment`;
+const USER_API = `${API_BASE_URL}/v1/api/auth`;
+const BOOK_API = `${API_BASE_URL}/v1/api/book`;
+const PAYMENT_API = `${API_BASE_URL}/v1/api/payment`;
 
 export { USER_API, BOOK_API, PAYMENT_API };
